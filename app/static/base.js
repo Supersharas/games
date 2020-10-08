6c6a539e080a5625c217b8cd28c0c915a05db7ec
jhon <supersharas@gmail.com>
2020-10-08 21:03:32 +0100

    slide in out

--- app/static/base.js
+++ app/static/base.js
@@ -1,6 +1,15 @@
 
+var slideOpen = false;
+
 
 function openSlide() {
   //document.getElementById('slide').style.left = '0';
-  document.querySelector('.slide').classList.add("open");
+  console.log('working');
+  if(slideOpen){
+  	document.querySelector('.slide').classList.remove("open");
+  	slideOpen = false;
+  } else {
+  	document.querySelector('.slide').classList.add("open");
+  	slideOpen = true;
+  }
 }
--- chess.py
+++ chess.py
@@ -1,11 +1,13 @@
 from app import app
 
 
+'''
+source env/bin/activate
+FLASK_APP=chess.py
+export FLASK_ENV=development
+flask run --host=0.0.0.0 --port=8080
 
-#source env/bin/activate
-#FLASK_APP=chess.py
-#export FLASK_ENV=development
-#flask run --host=0.0.0.0 --port=8080
+'''
 
 #sudo git push heroku master --force
 
